3de6753be5fc1f3154075e597c0a3b9c6e5e953c
tmflsby <823246840@qq.com>
2019-03-07 15:55:49 +0800

    ajax

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,14 +8,18 @@
       输入城市/景点/游玩主题
     </div>
     <div class="header-right">
-      城市<span class="iconfont arrow-icon">&#xe606;</span>
+      {{this.city}}
+      <span class="iconfont arrow-icon">&#xe606;</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeHeader'
+  name: 'HomeHeader',
+  props: {
+    city: String
+  }
 }
 </script>
 
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
-    <swiper-slide v-for="item of swiperList" :key="item.id">
+    <swiper :options="swiperOption" v-if="showSwiper">
+    <swiper-slide v-for="item of list" :key="item.id">
       <img class="swiper-img" :src="item.imgUrl" alt="">
     </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
@@ -12,26 +12,20 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: '//img1.qunarzz.com/vc/2a/96/76/d149c7a3a5b113f876f9a66e09.png'
-      }, {
-        id: '0002',
-        imgUrl: '//img1.qunarzz.com/vc/1d/d5/dc/bfb1990e14a1fdb082cf817657.jpg'
-      }, {
-        id: '0003',
-        imgUrl: '//img1.qunarzz.com/vc/a9/f4/f1/bb33ddb697f07c375e7af81256.jpg'
-      }, {
-        id: '0004',
-        imgUrl: '//img1.qunarzz.com/vc/bf/9d/a3/67b7b37511fa26a78298bf1da1.jpg'
       }
-      ]
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
